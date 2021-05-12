1660f0e2775f1337d3b5564103ad09ae324c3f24
Ömer Ügür <79576871+oemerueguer@users.noreply.github.com>
2021-05-12 15:56:06 +0200

    update solution

--- exercises6.js
+++ exercises6.js
@@ -2,3 +2,12 @@
 // In a file `exercise6.js`:
 // Install the module [axios](https://github.com/axios/axios) and retrieve the html code from `google.com`.
 
+const axios = require('axios');
+
+axios.get('http://google.com')
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+ });
