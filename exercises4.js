b596974d4aa819c517abaa69f5d4b596ebf5819f
Ömer Ügür <79576871+oemerueguer@users.noreply.github.com>
2021-05-12 15:48:03 +0200

    update solution

--- exercises4.js
+++ exercises4.js
@@ -1,1 +1,15 @@
+// Exercise 4 ([Help - fs](https://nodejs.org/api/fs.html))
+// In a file `exercise4.js`:
+// Write the work done in the previous exercises in ES5 or ES6 (depending which version you used).
+
+// you need to add functio,  delete =>,  and add throw to err
+
+const fs = require('fs');
+
+fs.readFile('test.html','utf8', function(err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+
 
