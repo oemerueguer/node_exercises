ff29741c17ab0506d73eb01ea16b394635ebcfef
Ömer Ügür <79576871+oemerueguer@users.noreply.github.com>
2021-05-12 15:42:29 +0200

    update solution

--- exercises3.js
+++ exercises3.js
@@ -2,6 +2,14 @@
 // In a file `exercise3.js`:
 // Using the `readFile` function, read the file `test.html` and display the content in the console.
 
+//readFile()
+const fs = require('fs');
+
+fs.readFile('test.html','utf8', (err, data) => {
+  if (err) throw err; // console.log(err)
+  console.log(data);
+});
+
 
 
 
