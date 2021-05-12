045dda14c7dd4e0f7b0e38d1b6fda2f4efab03c1
Ömer Ügür <79576871+oemerueguer@users.noreply.github.com>
2021-05-12 15:36:36 +0200

    update exercises1

--- exercises1.js
+++ exercises1.js
@@ -1,1 +1,7 @@
+// ## Exercise 1 ([Help - console](https://nodejs.org/api/console.html))
+// In a file `exercise1.js`:
+// Create a code that displays `hello` in the console.
+// To launch this file, type in your terminal `node exercise1.js`.
+
+
 console.log('hello');
