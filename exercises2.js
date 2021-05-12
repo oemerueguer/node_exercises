28aeac5bd4708bc31722a9b62eeb60c94017fd80
Ömer Ügür <79576871+oemerueguer@users.noreply.github.com>
2021-05-12 15:40:28 +0200

    update solution

--- exercises2.js
+++ exercises2.js
@@ -1,3 +1,10 @@
 // ## Exercise 2 ([Help - fs](https://nodejs.org/api/fs.html))
 // In a file `exercise2.js`:
 // Using the `writeFile` function, write `hello` in a file called `test.html`.
+
+const fs = require('fs');
+
+fs.writeFile('test.html', 'hello', (err) => {
+  if (err) throw err;
+  console.log('Saved!');
+});
