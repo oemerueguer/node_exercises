e55664169190216fc6159ad755891dc368535a7f
Ömer Ügür <79576871+oemerueguer@users.noreply.github.com>
2021-05-12 15:41:44 +0200

    update exercises3

--- exercises3.js
+++ exercises3.js
@@ -1,1 +1,7 @@
+// ## Exercise 3 ([Help - fs](https://nodejs.org/api/fs.html))
+// In a file `exercise3.js`:
+// Using the `readFile` function, read the file `test.html` and display the content in the console.
+
+
+
 
