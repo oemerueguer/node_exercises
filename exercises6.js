e3a5a676c5fa37afed6f1e6f34707832b74b017a
Ömer Ügür <79576871+oemerueguer@users.noreply.github.com>
2021-05-16 23:03:48 +0200

    update exercises6

--- exercises6.js
+++ exercises6.js
@@ -1,12 +1,16 @@
 // Exercise 6
 // In a file `exercise6.js`:
 // Install the module [axios](https://github.com/axios/axios) and retrieve the html code from `google.com`.
+// I created/ wrote a html file to save the outout. By using Prettier on VSCode, you can see the code formatted
 
 const axios = require('axios');
 
 axios.get('http://google.com')
   .then((response) => {
-    console.log(response);
+    fs.writeFile('google.html',  response.data, 'utf-8', (err) => {
+      if(err) throw err
+    })
+    //console.log(response);
   })
   .catch((error) => {
     console.log(error);
